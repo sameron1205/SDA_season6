f4b8c949b4ca32b188574b2f788682921a6cf36b
sameron1205 <sameron1205@gmail.com>
2023-05-25 19:36:57 +0900

    슬라이드 완료

--- script/script.js
+++ script/script.js
@@ -1,31 +1,53 @@
-// var swiper = new Swiper(".swiper1", {
-//   slidesPerView: 4.5,
-//   spaceBetween: 12,
-//   loop: true,
-//   centeredSlides: true,
-//   autoHeight : true,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-// });
-
-$(document).ready(function() {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    items: 4.5,
-    autoplay: true,
-    slideTransition: 'linear',
-    autoplaySpeed: 6000,
-    smartSpeed: 6000,
-    center: true,
-  });
-
-  $('.owl-carousel').trigger('play.owl.autoplay',[2000]);
-
-  function setSpeed(){
-      $('.owl-carousel').trigger('play.owl.autoplay',[6000]);
-  }
-
-  setTimeout(setSpeed, 1000);
+const sw1 = new Swiper(".sw1", {
+  slidesPerView: 4.5,
+  spaceBetween: 20,
+  loop: true,
+  centeredSlides: true,
+  speed: 6000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false
+  },
 });
+
+const sw2 = new Swiper( '.sw2', {
+	effect : 'fade',
+	loop : true,
+  speed: 1000,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+	navigation : {
+		nextEl : '.swiper-button-next',
+		prevEl : '.swiper-button-prev',
+	},
+});
+
+const sw3 = new Swiper(".sw3", {
+  slidesPerGroup : 4,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  // loopFillGroupWithBlank : true,
+  speed: 3000,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false
+  },
+  pagination : {
+		el : '.sw3-pagination',
+		clickable : true,
+	},
+});
+
+var cal = new FullCalendar.Calendar($('#cal')[0], {
+  initialView: 'dayGridMonth',
+  height: '550px',
+  headerToolbar: {
+    left: 'title prev,next',
+    center: '',
+    right: ''
+    },
+  });
+cal.render();
