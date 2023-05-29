1ace5c7208afbe511bb97cfe633c576cd30ac743
sameron1205 <sameron1205@gmail.com>
2023-05-29 19:03:18 +0900

    적응형 768, 1024 수정 및 모바일 side-nav 추가

--- script/script.js
+++ script/script.js
@@ -6,7 +6,12 @@ const sw1 = new Swiper(".sw1", {
   centeredSlides: true,
   breakpoints: {
     768: {
-      slidesPerView: 4.5,
+      slidesPerView: 4.8,
+      speed: 6000,
+      autoplay: { delay: 0 },
+    },
+    1024: {
+      slidesPerView: 4.8,
       spaceBetween: 20,
       speed: 6000,
       autoplay: { delay: 0 },
@@ -47,7 +52,7 @@ const sw3 = new Swiper(".sw3", {
       // loopFillGroupWithBlank : true,
       slidesPerGroup : 4,
       slidesPerView: 4,
-      spaceBetween: 20,
+      spaceBetween: 10,
       speed: 3000,
       autoplay: { delay: 6000 },
     },
@@ -367,6 +372,11 @@ const calendar = new FullCalendar.Calendar($('#calendar')[0], {
 });
 calendar.render();
 
+
+
+
+
+
 // 팝업
 const bt = $('#popup-bt')[0];
 const exit = () => {
@@ -378,69 +388,119 @@ const exit = () => {
 bt.onmouseout = function() { this.src = 'css/image/popup_bt1.png'; };
 bt.onmouseover = function() { this.src = 'css/image/popup_bt2.png'; };
 
-// 섹션 1
-function changeSec1() {
-  const sec1 = [$('#sec1-head'), $('#sec1-text'), $('#sec1-tail')];
-  const sec3 = [$('#sec3-head'), $('#sec3-text')];
-  const title = [$('.t1'), $('.t2')];
-  const footer = $('#footer-inner');
 
-  if(window.innerWidth > 768) {
-    sec1[0].html("미래의 도트 아티스트 여러분 환영해요!");
-    sec1[1].html(`슈퍼캣 도트 아카데미는 도트 기초 이론부터 실무형 과제에 이르기까지,<br>
-      도트 아티스트로서의 역량을 키워나가는 교육 프로그램입니다.<br>
-      <br>
-      현업에 종사하는 강사진의 수업을 통해,<br>
-      12주 동안 도트 아티스트로의 경험치를 쌓아갑니다.`);
-    sec1[2].html("지금, 슈퍼캣 도트 아카데미의 문을 열어주세요.");
 
-    sec3[0].html("채용연계 프로그램으로 <span style='color: black;'>슈퍼피플</span>이 되어보세요");
-    sec3[1].html(`우수한 성적으로 도트 아카데미를 수료한 수강생에게는<br>
-      슈퍼캣 도트 아티스트 정규 채용 기회가 제공됩니다.`);
 
-    title[0].html("SDA<br>다양한 혜택이 준비되어 있어요");
-    title[1].html("SDA<br>수강생 작품을 만나보세요");
 
-    footer.html(`<div id="info">
-        <div id="copyright">©SUPERCAT Inc. All Rights Reserved.</div>
-        <ul id="policy">
-          <li><a href="#">이용약관</a></li>
-          <li><a href="#" style="font-weight: bold;">개인정보처리방침</a></li>
-          <li><a href="#">슈퍼캣 채용</a></li>
-        </ul>
-      </div>
-      <a href="#">슈퍼캣 홈페이지</a>`);
+
+// 반응형
+const sec1 = ["미래의 도트 아티스트 여러분 환영해요!", 
+              `슈퍼캣 도트 아카데미는 도트 기초 이론부터 실무형 과제에 이르기까지,<br>
+              도트 아티스트로서의 역량을 키워나가는 교육 프로그램입니다.<br>
+              <br>
+              현업에 종사하는 강사진의 수업을 통해,<br>
+              12주 동안 도트 아티스트로의 경험치를 쌓아갑니다.`, 
+              "지금, 슈퍼캣 도트 아카데미의 문을 열어주세요."];
+const sec1_m = ["미래의 도트 아티스트<br>여러분 환영해요!", 
+                `슈퍼캣 도트 아카데미는 도트 기초 이론부터<br>
+                실무형 과제에 이르기까지, 도트 아티스트로서의<br>
+                역량을 키워나가는 교육 프로그램입니다.<br>
+                <br>
+                현업에 종사하는 강사진의 수업을 통해,<br>
+                12주 동안 도트 아티스트로의<br>
+                경험치를 쌓아갑니다.`, 
+                "지금, 슈퍼캣 도트 아카데미의<br>문을 열어주세요."];
+
+const sec3 = ["채용연계 프로그램으로 <span style='color: black;'>슈퍼피플</span>이 되어보세요", 
+              `우수한 성적으로 도트 아카데미를 수료한 수강생에게는<br>
+              슈퍼캣 도트 아티스트 정규 채용 기회가 제공됩니다.`];
+const sec3_m = ["채용연계 프로그램으로<br><span style='color: black;'>슈퍼피플</span>이 되어보세요", 
+                `우수한 성적으로 도트 아카데미를<br>
+                수료한 수강생에게는 슈퍼캣 도트 아티스트<br>
+                정규 채용 기회가 제공됩니다.`];
+
+const t = ["SDA<br>다양한 혜택이 준비되어 있어요", "SDA<br>수강생 작품을 만나보세요"];
+const t_m = ["SDA<br>다양한 혜택이<br>준비되어 있어요", "SDA<br>수강생 작품을<br>만나보세요"];
+
+const footer = `<div id="info">
+  <div id="copyright">©SUPERCAT Inc. All Rights Reserved.</div>
+  <ul id="policy">
+    <li><a href="#">이용약관</a></li>
+    <li><a href="#" style="font-weight: bold;">개인정보처리방침</a></li>
+    <li><a href="#">슈퍼캣 채용</a></li>
+  </ul>
+  </div>
+  <a href="#">슈퍼캣 홈페이지</a>`;
+const footer_m = `<ul id="policy">
+  <li><a href="#">이용약관</a></li>
+  <li><a href="#" style="font-weight: bold;">개인정보처리방침</a></li>
+  <li><a href="#">슈퍼캣 채용</a></li>
+  </ul>
+  <a href="#">슈퍼캣 홈페이지</a>
+  <div id="copyright">©SUPERCAT Inc. All Rights Reserved.</div>`;
+
+let isMobile = window.innerWidth < 768 ? false : true;
+
+function changeEl() {
+  if(isMobile && window.innerWidth >= 768) {
+    isMobile = false;
+
+    $('#sec1-head').html(sec1[0]);
+    $('#sec1-text').html(sec1[1]);
+    $('#sec1-tail').html(sec1[2]);
+
+    $('#sec3-head').html(sec3[0]);
+    $('#sec3-text').html(sec3[1]);
+
+    $('.t1').html(t[0]);
+    $('.t2').html(t[1]);
+
+    $('#footer-inner').html(footer);
+    
+    $('#side-bg').css('display', 'none');
+    $('#side-x').css('display', 'none');
+    $('#side-nav').attr('class', '');
+
+    // console.log('not mobile');
   }
-  else {
-    sec1[0].html("미래의 도트 아티스트<br>여러분 환영해요!");
-    sec1[1].html(`슈퍼캣 도트 아카데미는 도트 기초 이론부터<br>
-      실무형 과제에 이르기까지, 도트 아티스트로서의<br>
-      역량을 키워나가는 교육 프로그램입니다.<br>
-      <br>
-      현업에 종사하는 강사진의 수업을 통해,<br>
-      12주 동안 도트 아티스트로의<br>
-      경험치를 쌓아갑니다.`);
-    sec1[2].html("지금, 슈퍼캣 도트 아카데미의<br>문을 열어주세요.");
+  if(!isMobile && window.innerWidth < 768) {
+    isMobile = true;
 
-    sec3[0].html("채용연계 프로그램으로<br><span style='color: black;'>슈퍼피플</span>이 되어보세요");
-    sec3[1].html(`우수한 성적으로 도트 아카데미를<br>
-      수료한 수강생에게는 슈퍼캣 도트 아티스트<br>
-      정규 채용 기회가 제공됩니다.`);
+    $('#sec1-head').html(sec1_m[0]);
+    $('#sec1-text').html(sec1_m[1]);
+    $('#sec1-tail').html(sec1_m[2]);
 
-    title[0].html("SDA<br>다양한 혜택이<br>준비되어 있어요");
-    title[1].html("SDA<br>수강생 작품을<br>만나보세요");
+    $('#sec3-head').html(sec3_m[0]);
+    $('#sec3-text').html(sec3_m[1]);
 
-    footer.html(`<ul id="policy">
-          <li><a href="#">이용약관</a></li>
-          <li><a href="#" style="font-weight: bold;">개인정보처리방침</a></li>
-          <li><a href="#">슈퍼캣 채용</a></li>
-        </ul>
-      <a href="#">슈퍼캣 홈페이지</a>
-      <div id="copyright">©SUPERCAT Inc. All Rights Reserved.</div>`);
+    $('.t1').html(t_m[0]);
+    $('.t2').html(t_m[1]);
+
+    $('#footer-inner').html(footer_m);
+
+    // console.log('mobile');
   }
-
-  // $('html').css('font-size', `${window.innerWidth / 24}px`)
 }
 
-window.onload = function() { changeSec1(); }
-window.onresize = function() { changeSec1(); }
+window.onload = () => changeEl();
+window.onresize = () => changeEl();
+
+
+
+
+
+
+// 사이드 내비게이션
+function sideOpen() {
+  $('#side-bg').css('display', 'inline');
+  
+  $('#side-x').css('display', 'inline');
+  $('#side-nav').attr('class', 'slide-in');
+}
+
+function sideClose() {
+  $('#side-bg').css('display', 'none');
+
+  $('#side-x').css('display', 'none');
+  $('#side-nav').attr('class', 'slide-out');
+}
